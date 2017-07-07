a0d2ddb1b1de2cdc4e67aa1c5aa84aec0bcefa44
Kate Byars <katebyars5@gmail.com>
2017-07-07 14:57:38 -0700

    add a fifth question.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var add = function(answer1, answer2, answer3, answer4) {
- return answer1 + answer2 + answer3 + answer4;
+var add = function(answer1, answer2, answer3, answer4, answer5) {
+ return answer1 + answer2 + answer3 + answer4 + answer5;
 };
 
 
@@ -11,9 +11,11 @@ $(document).ready(function() {
     var answer2 = parseInt($("#question2").val());
     var answer3 = parseInt($("#question3").val());
     var answer4 = parseInt($("#question4").val());
-    var sum = add(answer1, answer2, answer3, answer4);
+    var answer5 = parseInt($("#question5").val());
 
-    if (sum<6){
+    var sum = add(answer1, answer2, answer3, answer4, answer5);
+
+    if (sum<7){
         $('#java').show();
         $('#ruby').hide();
         $('#php').hide();
@@ -23,18 +25,41 @@ $(document).ready(function() {
         $('#phpcsharp').hide();
         $("#formOne").hide();
         $("#secondrow").hide();
-      } else if (sum>6&&sum<10) {
-        $('#ruby').show();
+
+      } else if (sum>6&&sum<9) {
+        $('#javaruby').hide();
+        $('#ruby').hide();
         $('#java').hide();
         $('#php').hide();
         $('#csharp').hide();
-        $('#javaruby').hide();
         $('#rubyphp').hide();
         $('#phpcsharp').hide();
         $("#formOne").hide();
         $("#secondrow").hide();
 
-      } else if (sum>10&&sum<14) {
+      } else if (sum>8&&sum<12) {
+        $('#php').hide();
+        $('#ruby').show();
+        $('#java').hide();
+        $('#csharp').hide();
+        $('#javaruby').hide();
+        $('#rubyphp').hide();
+        $('#phpcsharp').hide();
+        $('#formOne').hide();
+        $("#secondrow").hide();
+
+      } else if (sum>11&&sum<14) {
+        $('#php').hide();
+        $('#ruby').hide();
+        $('#java').hide();
+        $('#csharp').hide();
+        $('#javaruby').hide();
+        $('#rubyphp').show();
+        $('#phpcsharp').hide();
+        $('#formOne').hide();
+        $("#secondrow").hide();
+
+      } else if (sum>13&&sum<17) {
         $('#php').show();
         $('#ruby').hide();
         $('#java').hide();
@@ -45,47 +70,14 @@ $(document).ready(function() {
         $('#formOne').hide();
         $("#secondrow").hide();
 
-      } else if (sum==4) {
-        $('#java').show();
-        $('#ruby').hide();
+      } else if (sum>16&&sum<19) {
         $('#php').hide();
-        $('#csharp').hide();
-        $('#rubyphp').hide();
-        $('#phpcsharp').hide();
-        $('#javaruby').hide();
-        $('#formOne').hide();
-        $("#secondrow").hide();
-
-      } else if (sum==8) {
-        $('#ruby').show();
-        $('#java').hide();
-        $('#php').hide();
-        $('#csharp').hide();
-        $('#rubyphp').hide();
-        $('#phpcsharp').hide();
-        $('#javaruby').hide();
-        $('#formOne').hide();
-        $("#secondrow").hide();
-
-      } else if (sum==12) {
-        $('#php').show();
         $('#ruby').hide();
         $('#java').hide();
         $('#csharp').hide();
-        $('#rubyphp').hide();
-        $('#phpcsharp').hide();
         $('#javaruby').hide();
-        $('#formOne').hide();
-        $("#secondrow").hide();
-
-      } else if (sum==16) {
-        $('#csharp').show();
-        $('#ruby').hide();
-        $('#java').hide();
-        $('#php').hide();
         $('#rubyphp').hide();
-        $('#phpcsharp').hide();
-        $('#javaruby').hide();
+        $('#phpcsharp').show();
         $('#formOne').hide();
         $("#secondrow").hide();
 
